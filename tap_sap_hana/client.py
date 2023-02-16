63ef7bcb4c8ece599a5f0fbd7c4bfda3085f7317
Dimitar Petrunov <dimitar.petrunov@sagedata.net>
2023-02-16 14:03:42 +0200

    Fix for DATE columns, treat them as varchars

--- tap_sap_hana/client.py
+++ tap_sap_hana/client.py
@@ -89,58 +89,26 @@ class SapHanaConnector(SQLConnector):
 
         return sqlalchemy.engine_from_config(eng_config, prefix=eng_prefix)
 
-    # @staticmethod
-    # def to_jsonschema_type(sql_type: sqlalchemy.types.TypeEngine) -> dict:
-    #     """Returns a JSON Schema equivalent for the given SQL type.
+    @staticmethod
+    def to_jsonschema_type(sql_type: sqlalchemy.types.TypeEngine) -> dict:
+        """Returns a JSON Schema equivalent for the given SQL type.
 
-    #     Developers may optionally add custom logic before calling the default
-    #     implementation inherited from the base class.
-    #     """
+        Developers may optionally add custom logic before calling the default
+        implementation inherited from the base class.
+        """
 
-    #     """
-    #     Checks for the HANA type of NUMERIC
-    #         if scale = 0 it is typed as a INTEGER
-    #         if scale != 0 it is typed as NUMBER
-    #     """
-    #     if str(sql_type).startswith("NUMERIC"):
-    #         if str(sql_type).endswith(", 0)"):
-    #            sql_type = "int"
-    #         else:
-    #            sql_type = "number"
+        """
+        Checks for the HANA type of NUMERIC
+            if scale = 0 it is typed as a INTEGER
+            if scale != 0 it is typed as NUMBER
+        """
 
-    #     if str(sql_type) in ["MONEY", "SMALLMONEY"]:
-    #         sql_type = "number"
+        # singer_sdk.typing appends T00:00:00Z to dates ? which breaks destination into delivery tables for redshift
+        # this is a workaround
+        if str(sql_type) == "DATE":
+            sql_type = "varchar"
 
-    #     return SQLConnector.to_jsonschema_type(sql_type)
-
-    # @staticmethod
-    # def to_sql_type(jsonschema_type: dict) -> sqlalchemy.types.TypeEngine:
-    #     """Returns a JSON Schema equivalent for the given SQL type.
-
-    #     Developers may optionally add custom logic before calling the default
-    #     implementation inherited from the base class.
-    #     """
-    #     # Optionally, add custom logic before calling the parent SQLConnector method.
-    #     # You may delete this method if overrides are not needed.
-    #     return SQLConnector.to_sql_type(jsonschema_type)
-
-
-# Custom class extends json.JSONEncoder
-# class CustomJSONEncoder(json.JSONEncoder):
-
-#     # Override default() method
-#     def default(self, obj):
-
-#         # Datetime to string
-#         if isinstance(obj, datetime):
-#             # Format datetime - `Fri, 21 Aug 2020 17:59:59 GMT`
-#             #obj = obj.strftime('%a, %d %b %Y %H:%M:%S GMT')
-#             obj = pendulum.instance(obj).isoformat()
-#             return obj
-
-#         # Default behavior for all other types
-#         return super().default(obj)
-
+        return SQLConnector.to_jsonschema_type(sql_type)
 
 class SapHanaStream(SQLStream):
     """Stream class for sap hana streams."""
@@ -194,45 +162,3 @@ class SapHanaStream(SQLStream):
 
         for record in self.connector.connection.execute(query):
             yield dict(record)
-
-    def get_batches(
-        self,
-        batch_config: BatchConfig,
-        context: dict | None = None,
-        ) -> Iterable[tuple[BaseBatchFileEncoding, list[str]]]:
-        """Batch generator function.
-
-        Developers are encouraged to override this method to customize batching
-        behavior for databases, bulk APIs, etc.
-
-        Args:
-            batch_config: Batch config for this stream.
-            context: Stream partition or context dictionary.
-
-        Yields:
-            A tuple of (encoding, manifest) for each batch.
-        """
-        sync_id = f"{self.tap_name}--{self.name}-{uuid4()}"
-        prefix = batch_config.storage.prefix or ""
-
-        for i, chunk in enumerate(
-            lazy_chunked_generator(
-                self._sync_records(context, write_messages=False),
-                self.batch_size,
-            ),
-            start=1,
-        ):
-            filename = f"{prefix}{sync_id}-{i}.json.gz"
-            with batch_config.storage.fs() as fs:
-                with fs.open(filename, "wb") as f:
-                    # TODO: Determine compression from config.
-                    with gzip.GzipFile(fileobj=f, mode="wb") as gz:
-                        gz.writelines(
-                            (json.dumps(record,
-                            default=str,
-                            # cls=CustomJSONEncoder,
-                            ) + "\n").encode() for record in chunk
-                        )
-                file_url = fs.geturl(filename)
-
-            yield batch_config.encoding, [file_url]
